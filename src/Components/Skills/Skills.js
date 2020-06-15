83994cc9b202c72ec4b77878f6b13e27461308cf
Shivang Dave <daveshivang15@gmail.com>
2020-06-15 17:39:54 -0400

    Added graph for skills..

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,65 +1,11 @@
 import React, { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
+import {InteractiveForceGraph, ForceGraphNode, ForceGraphLink } from 'react-vis-force';
 
-import HighlightedSkills from './HighlightedSkills';
+import skills from './static/skills';
 import ResumeBtn from './ResumeBtn';
 
 export default ({ setSection }) => {
-
-  const language = [
-    'Ruby',
-    'Ruby on Rails',
-    'Python',
-    'JavaScript',
-    'ReactJS',
-    'Redux',
-    'Node',
-    'React Native',
-    'HTML',
-    'CSS',
-    'Swift',
-    'Git',
-    'SQL',
-    'Postgres',
-    'MongoDB',
-    'AWS',
-    'Google Cloud',
-    'Heroku',
-    'Digital Ocean',
-    'Docker',
-    'KVM',
-    'Framer Motion'
-  ]
-  const percentage = [
-    95,
-    94,
-    80,
-    97,
-    97,
-    90,
-    92,
-    85,
-    92,
-    90,
-    80,
-    95,
-    90,
-    90,
-    83,
-    84,
-    84,
-    70,
-    80,
-    92,
-    70,
-    80
-  ]
-
-  const highlightedSkills = [
-    'Full Stack',
-    'Mobile Apps',
-    'Educator'
-  ]
 
   const [ref,inView] = useInView({ threshold: 0.6 });
 
@@ -70,11 +16,60 @@ export default ({ setSection }) => {
     }
   },[inView,setSection])
 
+  const isBigger = (val) => {
+    let returnVal = false
+    if(val === 'Software Engineer'){
+      returnVal = true
+    }else if(val === 'Educator'){
+      returnVal = true
+    }else if(val === 'Full Stack (Web)'){
+      returnVal = true
+    }
+    return returnVal
+  }
+
   return (
     <div ref={ref} id={'skills'} className={'skills-container'}>
-        <HighlightedSkills key={'large'} arr={highlightedSkills} />
-        <HighlightedSkills key={'small'} arr={language} val={percentage} />
-        <ResumeBtn />
+      <div className={'graph-container'}>
+        <InteractiveForceGraph
+          simulationOptions={{
+            strength: {
+              charge: -500,
+              collide: 8
+            },
+            animate: true,
+            alpha: 0.01,
+            radiusMargin: 0,
+            height: '800',
+            width: '800'
+          }}
+        >
+          {
+            skills.nodes.map(node =>
+              <ForceGraphNode
+                key={node.id}
+                node={{ id: node.id }}
+                showLabel
+                r={isBigger(node.id) ? 45 : 20}
+                strokeWidth={4}
+                fill="rgb(253,177,177,1)"
+              />
+            )
+          }
+          {
+            skills.links.map((link,index) =>
+              <ForceGraphLink
+                key={index}
+                fill={'white'}
+                strokeWidth={2}
+                link={{ source: link.source, target: link.target }}
+              />
+            )
+          }
+        </InteractiveForceGraph>
+      </div>
+
+      <ResumeBtn />
     </div>
   )
 }
